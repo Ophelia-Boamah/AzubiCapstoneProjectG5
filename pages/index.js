a26b1cc98ad4a314779ba751267aa81a3b25e4c4
Ophelia-Boamah <ophybee8@gmail.com>
2020-08-31 23:33:52 +0000

    Project setup

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,23 @@
-import { Box } from "@chakra-ui/core";
+import { Box, Input, Flex, Heading } from "@chakra-ui/core";
 
 export default function Home() {
-  return <Box>Hi there!!!!!!!!!!</Box>;
+  return (
+    <Box bg="blue.400" h="100vh">
+      <Flex
+        bg="#fff"
+        w="50%"
+        h="50%"
+        mx="auto"
+        p={{ md: 20 }}
+        align="center"
+        justify="center"
+        flexDir="column"
+      >
+        <Heading mb={10}>Sign in to your account</Heading>
+        <Input placeholder="Email address" />
+        <br />
+        <Input placeholder="Password" />
+      </Flex>
+    </Box>
+  );
 }
