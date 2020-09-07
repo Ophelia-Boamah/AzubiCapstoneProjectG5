c3a10544d3da3da60d003c19cc481dd123b9a348
Ophelia-Boamah <ophybee8@gmail.com>
2020-09-07 00:36:21 +0000

    Form control setup

--- pages/signin.js
+++ pages/signin.js
@@ -15,10 +15,10 @@ import { Formik } from "formik";
 const Signin = () => {
   return (
     <Box overflowX="hidden">
-      <Box bg="green.400" width="100%" height="100vh">
+      <Box bg="#fff" width="100%" height="100vh">
         <Flex height="100%" align="center" justify="center">
           <Flex
-            bg="white"
+            bg="gray.50"
             width="500px"
             borderRadius="lg"
             p={20}
@@ -31,49 +31,62 @@ const Signin = () => {
               Sign in to your account
             </Heading>
             <Formik
-            // enableReinitialize
-            // initialValues={{ UserCode: data.Usercode, Url: data.Url }}
-            // onSubmit={onSubmit}
+              enableReinitialize
+              initialValues={{ email: "", password: "" }}
+              onSubmit={(values, { setSubmitting }) => {
+                setTimeout(() => {
+                  alert(JSON.stringify(values, null, 2));
+                  setSubmitting(false);
+                }, 400);
+              }}
             >
-              <Box width="100%">
-                <form>
-                  <FormLabel htmlFor="number">Your Email</FormLabel>
-                  <Input
-                    type="email"
-                    name="Email"
-                    // value={values.UserCode}
-                    // onChange={handleChange}
-                    // onBlur={handleBlur}
-                    aria-describedby="userCode-helper-text"
-                  />
-                  <FormLabel htmlFor="url" mt={4}>
-                    Your Password
-                  </FormLabel>
-                  <Input
-                    type="password"
-                    name="Password"
-                    // value={values.Url}
-                    // onChange={handleChange}
-                    // onBlur={handleBlur}
-                    aria-describedby="url-helper-text"
-                  />
-                  <Button
-                    type="submit"
-                    width="100%"
-                    bg="blue.600"
-                    aria-label="submit button"
-                    mt={20}
-                    color="#fff"
-                    // isLoading={isSubmitting}
-                  >
-                    Sign in to your account
-                  </Button>
-                  <Text borderTop="1px solid #888" p={4} mt={6}>
-                    Don't have an account?{" "}
-                    <Link color="blue.600">Sign Up!</Link>
-                  </Text>
-                </form>
-              </Box>
+              {({
+                handleBlur,
+                handleChange,
+                isSubmitting,
+                values,
+                handleSubmit,
+              }) => (
+                <Box width="100%">
+                  <form onSubmit={handleSubmit}>
+                    <FormLabel htmlFor="number">Your Email</FormLabel>
+                    <Input
+                      type="email"
+                      name="email"
+                      value={values.email}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      aria-describedby="userCode-helper-text"
+                    />
+                    <FormLabel htmlFor="url" mt={4}>
+                      Your Password
+                    </FormLabel>
+                    <Input
+                      type="password"
+                      name="password"
+                      value={values.password}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      aria-describedby="url-helper-text"
+                    />
+                    <Button
+                      type="submit"
+                      width="100%"
+                      bg="blue.600"
+                      aria-label="submit button"
+                      mt={20}
+                      color="#fff"
+                      isLoading={isSubmitting}
+                    >
+                      Sign in to your account
+                    </Button>
+                    <Text borderTop="1px solid #888" p={4} mt={6}>
+                      Don't have an account?{" "}
+                      <Link color="blue.600">Sign Up!</Link>
+                    </Text>
+                  </form>
+                </Box>
+              )}
             </Formik>
           </Flex>
         </Flex>
