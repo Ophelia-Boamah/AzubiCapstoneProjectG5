37a2b2405bf0530512bb54e62bd847a9f3672e5f
Felix Nana Yaw Yeboah - Jefferson <gabsco208309@hotmail.com>
2020-09-18 08:58:41 +0000

    event details page

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,13 @@
 import '../styles/globals.css';
 import { Box, ChakraProvider, CSSReset } from '@chakra-ui/core';
 import { Provider as StyletronProvider } from 'styletron-react';
+import { useRouter } from 'next/router';
 import { styletron, debug } from '../styletron';
 
 import Navbar from '../utils/Navbars/Navbar';
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
   return (
     <ChakraProvider>
       <StyletronProvider value={styletron} debug={debug} debugAfterHydration>
@@ -14,6 +16,19 @@ function MyApp({ Component, pageProps }) {
         <Box>
           <Component {...pageProps} />
         </Box>
+        {router.pathname === '/' ? (
+          ''
+        ) : (
+          <Box
+            as='footer'
+            mt={{ md: 16 }}
+            mb={6}
+            px={{ md: 20 }}
+            textAlign='center'
+          >
+            &copy; 2020. A-Teo. All rights reserved.
+          </Box>
+        )}
       </StyletronProvider>
     </ChakraProvider>
   );
